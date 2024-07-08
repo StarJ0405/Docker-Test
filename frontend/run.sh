200504b1c0b26e0745ab2f5b43d7bc604b1f1238
StarJ0405 <ghdtjdwo126@gmail.com>
2024-07-08 13:04:20 +0900

    try

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-FRONT_PID=$(ps -ef | grep next-server | awk '{print $2}')
-
-
-if [ -z "$FRONT_PID" ];
-
-then
-
-    echo "FRONT_FRONT is not running"
-
-else
-
-    kill -9 $FRONT_PID
-
-    echo "FRONT_FRONT stopped."
-	
-fi
-LOG=/home/ubuntu/honeybadger/front.log
+LOG=/front.log
 
 echo "FRONT_FRONT started."
 
