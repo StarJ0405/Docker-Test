7cb21741c5f0c0f4deaf5f72cbadb5c89f9f8780
StarJ0405 <ghdtjdwo126@gmail.com>
2024-07-10 20:55:32 +0900

    change test

--- build_sub.sh
+++
@@ -1,8 +0,0 @@
-response=$(curl -s localhost:8081/api/track/health)
-if [[ $response =~ "UP" ]]; then
-  # 서비스가 실행 중인 경우
-  echo "서비스가 실행 중입니다."
-else
-  # 서비스가 실행 중이지 않은 경우
-  echo "서비스가 실행 중이지 않습니다."
-fi
